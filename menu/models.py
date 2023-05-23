72064df601f9e2b162bf6853dd7ecb9ff6bbdf21
IlyaD <kapamepuka@gmail.com>
2023-05-23 16:44:26 +0300

    index

--- menu/models.py
+++ menu/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField( verbose_name='Название', max_length = 50)
+    name = models.CharField(verbose_name='Название', max_length = 50)
     description = models.TextField(verbose_name='Описание')
     price = models.IntegerField(verbose_name='Цена')
     photo = models.ImageField(verbose_name="Фото товара", upload_to="products")
