103b7159383bc03e769a7580ebff1d1c5f4d71ba
0xshodan <hngdean@gmail.com>
2023-06-12 16:54:35 +0300

    pre-lesson

--- diploma/urls.py
+++ diploma/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from menu.views import MenuView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("menu/", MenuView.as_view()),
     path("cart/", include("cart.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
